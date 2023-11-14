004b603d91d5ff83f0376838a0db646f057dfcbf
Isaac Shoebottom <ir.shoebottom@gmail.com>
2023-11-13 23:26:00 -0400

    FTP server uses venv

--- SimpleFTP/SimpleFTP.ps1
+++ SimpleFTP/SimpleFTP.ps1
@@ -1,5 +1,16 @@
-# Is working for video files?
-python3 -m pyftpdlib
+# Create virtual environment
+if (-not (Test-Path venv)) {
+	virtualenv venv
+}
+
+# Activate virtual environment
+.\venv\Scripts\activate.ps1
+
+# Install dependencies
+pip install pyftpdlib
+
+# Run FTP server
+python -m pyftpdlib
 
 # Not working for streaming video files
 # twistd -n ftp -r .
