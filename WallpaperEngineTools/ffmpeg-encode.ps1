3d4207467abc52445f12738a9764a16bb512d92b
Isaac Shoebottom <ir.shoebottom@gmail.com>
2023-11-21 00:12:55 -0400

    Reencode wallpaper engine files

---
+++ WallpaperEngineTools/ffmpeg-encode.ps1
@@ -0,0 +1,4 @@
+# Rencode arg file 1 with ffmpeg crf 20 and veryslow preset in x264 format, and audio with aac 192kbps and append "-Small" to the output filename
+# Usage: ffmpeg-encode.ps1 arg1 arg2
+$file = Get-ChildItem $args[0]
+ffmpeg -i $file.FullName -c:v libx264 -preset veryslow -crf 20 -c:a aac -b:a 192k "$($file.BaseName)-Small.mp4"
