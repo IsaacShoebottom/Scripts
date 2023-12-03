b352f4eff621e595fbe3892f1d5d43b47c8a47a2
Isaac Shoebottom <ir.shoebottom@gmail.com>
2023-12-02 23:39:50 -0400

    Script that just defines hq defaults for streaming

---
+++ scrcpy-default/scrcpy-default.ps1
@@ -0,0 +1,2 @@
+# Want higher bitrate, 20Mbps, and to use AAC audio codec at 320kbps, as well as up to 144 fps
+scrcpy --video-bit-rate=20M --audio-codec aac --audio-bit-rate=320K --max-fps 144 --turn-screen-off
