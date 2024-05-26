0085d4dee2f31a3991758bc9d0804394a2ea3018
Isaac Shoebottom <ir.shoebottom@gmail.com>
2024-05-25 21:28:32 -0300

    add f4 toggle script

---
+++ SteamToggleUpdate/ToggleFallout4Updates.ps1
@@ -0,0 +1,20 @@
+$appId = 377160
+$networkLocation = "https://raw.githubusercontent.com/IsaacShoebottom/Scripts/master/SteamToggleUpdate/ToggleUpdates.ps1"
+
+# Test if ToggleUpdates.ps1 exists and invoke it if it does
+If (Test-Path -Path ".\ToggleUpdates.ps1" -PathType Leaf) {
+	.\ToggleUpdates.ps1 $appId
+}
+Else {
+	# construct temp path
+	$env:temp = [System.IO.Path]::GetTempPath()
+	$tempPath = $env:temp + "\ToggleUpdates.ps1"
+
+	# Download the network script
+	$wc = New-Object System.Net.WebClient
+	$wc.DownloadFile($networkLocation, $tempPath)
+	# Invoke the network script
+	&$tempPath $appId
+	# Delete the network script
+	Remove-Item -Path $tempPath
+}
